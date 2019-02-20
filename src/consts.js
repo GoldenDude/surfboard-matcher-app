62663c115c37c19a5b1ec4708e564c2084b86664
GoldenDude <edanazran@gmail.com>
2019-02-20 11:45:04 +0200

    1.5

--- src/consts.js
+++ src/consts.js
@@ -5,5 +5,5 @@ export default {
     PRODUCTS_SHOWN: 8,
     MAX_WAVE_HEIGHT: 4,
     SERVICE_URL: 'https://surfboard-matcher.herokuapp.com',
-
+    GOOGLE_CLIENT_ID: '366517766809-oktrgpvmmhneovtvi5a1q08cos7ahr66.apps.googleusercontent.com'
 }
