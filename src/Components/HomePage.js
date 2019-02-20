11a1613edcfbc2e830bb671696f60f1038338b1d
GoldenDude <edanazran@gmail.com>
2019-02-20 12:56:30 +0200

    t

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -24,7 +24,7 @@ class HomePage extends Component{
         this.socket     = openSocket(consts.SERVICE_URL);
         
         this.state = {
-            logged: false,
+            logged: false
         }
                 
         this.renderLogged        = this.renderLogged.bind(this);
